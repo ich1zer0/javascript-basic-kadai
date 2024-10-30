9554631389059455137a835740f842bcc32df42a
ich1zer0 <proton.ich1@protonmail.com>
2024-10-30 09:25:55 +0900

    feat 条件分岐を使用して倍数を表示しよう 課題

---
+++ kadai_007/calc.js
@@ -0,0 +1,11 @@
+const num = 15;
+
+if (num % 15 === 0) {
+  console.log("3と5の倍数です");
+} else if (num % 3 === 0) {
+  console.log("3の倍数です");
+} else if (num % 5 === 0) {
+  console.log("5の倍数ですす");
+} else {
+  console.log(num);
+}
